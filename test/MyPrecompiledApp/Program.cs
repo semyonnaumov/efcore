7dabd7f1ea5e5acdcc53cc36f8a935a788661681
maumar <maumar@microsoft.com>
2024-05-24 18:03:03 -0700

    kinda worx

--- test/MyPrecompiledApp/Program.cs
+++ test/MyPrecompiledApp/Program.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.EntityFrameworkCore;
-
+//using MyPrecompiledApp.Generated;
 namespace MyPrecompiledApp;
 
 internal class Program
@@ -12,8 +12,16 @@ internal class Program
         using var ctx = new MyContext();
         //ctx.Database.EnsureDeleted();
         //ctx.Database.EnsureCreated();
+        //var ctx_Entities = ctx.Set<MyEntity>().AsNoTracking();
+        var query = ctx.Set<MyEntity>().AsNoTracking().Where(x => x.Id > 5).ToList();
 
-        var query = ctx.Entities.Where(x => x.Id > 5).ToList();
+
+
+        foreach (var result in query)
+        {
+            Console.WriteLine("Id: " + result.Id + " Name: " + result.Name);
+        }
+
     }
 }
 
@@ -23,16 +31,18 @@ internal class Program
 public class MyContext : DbContext
 {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-    public DbSet<MyEntity> Entities { get; set; }
+    //public DbSet<MyEntity> Entities { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<MyEntity>().ToTable("Entities");
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Repro;Trusted_Connection=True;MultipleActiveResultSets=true");
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Repro;Trusted_Connection=True;MultipleActiveResultSets=true");//.UseModel(MyContextModel.Instance);
+
     }
 }
 

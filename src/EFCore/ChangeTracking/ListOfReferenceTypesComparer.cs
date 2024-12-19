0ec175bb5c3e087a153c83f6da73cdf7b91ed3a5
maumar <maumar@microsoft.com>
2024-12-18 17:56:59 -0800

    improved

--- src/EFCore/ChangeTracking/ListOfReferenceTypesComparer.cs
+++ src/EFCore/ChangeTracking/ListOfReferenceTypesComparer.cs
@@ -30,22 +30,13 @@ public sealed class ListOfReferenceTypesComparer<TConcreteList, TElement> : Valu
             && typeof(TConcreteList).GetGenericTypeDefinition() == typeof(ReadOnlyCollection<>));
 
     private static readonly MethodInfo CompareMethod = typeof(ListOfReferenceTypesComparer<TConcreteList, TElement>).GetMethod(
-        nameof(Compare), BindingFlags.Static | BindingFlags.NonPublic, [typeof(object), typeof(object), typeof(Func<object, object, bool>)])!;
-
-    private static readonly MethodInfo TypedCompareMethod = typeof(ListOfReferenceTypesComparer<TConcreteList, TElement>).GetMethod(
-        nameof(TypedCompare), BindingFlags.Static | BindingFlags.NonPublic, [typeof(object), typeof(object), typeof(Func<TElement, TElement, bool>)])!;
+        nameof(Compare), BindingFlags.Static | BindingFlags.NonPublic, [typeof(object), typeof(object), typeof(Func<TElement, TElement, bool>)])!;
 
     private static readonly MethodInfo GetHashCodeMethod = typeof(ListOfReferenceTypesComparer<TConcreteList, TElement>).GetMethod(
-        nameof(GetHashCode), BindingFlags.Static | BindingFlags.NonPublic, [typeof(IEnumerable), typeof(Func<object, int>)])!;
-
-    private static readonly MethodInfo TypedGetHashCodeMethod = typeof(ListOfReferenceTypesComparer<TConcreteList, TElement>).GetMethod(
-        nameof(TypedGetHashCode), BindingFlags.Static | BindingFlags.NonPublic, [typeof(IEnumerable), typeof(Func<TElement, int>)])!;
+        nameof(GetHashCode), BindingFlags.Static | BindingFlags.NonPublic, [typeof(IEnumerable), typeof(Func<TElement, int>)])!;
 
     private static readonly MethodInfo SnapshotMethod = typeof(ListOfReferenceTypesComparer<TConcreteList, TElement>).GetMethod(
-        nameof(Snapshot), BindingFlags.Static | BindingFlags.NonPublic, [typeof(object), typeof(Func<object, object>)])!;
-
-    private static readonly MethodInfo TypedSnapshotMethod = typeof(ListOfReferenceTypesComparer<TConcreteList, TElement>).GetMethod(
-        nameof(TypedSnapshot), BindingFlags.Static | BindingFlags.NonPublic, [typeof(object), typeof(Func<TElement, TElement>)])!;
+        nameof(Snapshot), BindingFlags.Static | BindingFlags.NonPublic, [typeof(object), typeof(Func<TElement, TElement>)])!;
 
     /// <summary>
     ///     Creates a new instance of the list comparer.
@@ -71,9 +62,12 @@ public sealed class ListOfReferenceTypesComparer<TConcreteList, TElement> : Valu
         var prm1 = Expression.Parameter(typeof(object), "a");
         var prm2 = Expression.Parameter(typeof(object), "b");
 
-        // nested collection case - TElement of element comparer is typed as object
-        // but TElement of (this) list comparer is likely something else so we are using untyped overload of Compare
-        if (elementComparer is ValueComparer<object>)
+        // we check the compatibility of expected equals lambda signature Func<TElement, TElement, bool>
+        // vs method we actually get from the element comparer, which would be typed to its generic argument
+        // if the expected is assignable from actual we can just do simple call...
+        var expectedSignature = typeof(Func<TElement, TElement, bool>);
+        var actualSignature = typeof(Func<,,>).MakeGenericType(elementComparer.Type, elementComparer.Type, typeof(bool));
+        if (expectedSignature.IsAssignableFrom(actualSignature))
         {
             return Expression.Lambda<Func<object?, object?, bool>>(
                 Expression.Call(
@@ -85,44 +79,26 @@ public sealed class ListOfReferenceTypesComparer<TConcreteList, TElement> : Valu
                 prm2);
         }
 
-        // special case for object[] { 1, 2, 3 }
-        // TElement of element comparer is int, so its equals method is typed as Func<int, int, bool>
-        // which doesn't fit the expected Func<object, object, bool>
-        // rewriting the inner lambda parameters to objects and cast to proper type in the body so that semantics are the same
-        if (typeof(TElement) == typeof(object))
-        {
-            var newInnerPrm1 = Expression.Parameter(typeof(object), "a");
-            var newInnerPrm2 = Expression.Parameter(typeof(object), "b");
+        // ...otherwise we need to rewrite the actual lambda (as we can't change the expected signature)
+        // in that case we are rewriting the inner lambda parameters to TElement and cast to the element comparer
+        // type argument in the body, so that semantics of the element comparison func don't change
+        var newInnerPrm1 = Expression.Parameter(typeof(TElement), "a");
+        var newInnerPrm2 = Expression.Parameter(typeof(TElement), "b");
 
-            var newEqualsExpressionBody = ReplacingExpressionVisitor.Replace(
-                elementComparer.EqualsExpression.Parameters,
-                [Expression.Convert(newInnerPrm1, elementComparer.Type), Expression.Convert(newInnerPrm2, elementComparer.Type)],
-                elementComparer.EqualsExpression.Body);
+        var newEqualsExpressionBody = ReplacingExpressionVisitor.Replace(
+            elementComparer.EqualsExpression.Parameters,
+            [Expression.Convert(newInnerPrm1, elementComparer.Type), Expression.Convert(newInnerPrm2, elementComparer.Type)],
+            elementComparer.EqualsExpression.Body);
 
-            return Expression.Lambda<Func<object?, object?, bool>>(
-                Expression.Call(
-                    TypedCompareMethod,
-                    prm1,
-                    prm2,
-                    Expression.Lambda(
-                        newEqualsExpressionBody,
-                        newInnerPrm1,
-                        newInnerPrm2)),
-                prm1,
-                prm2);
-        }
-
-        // what's left is either collection of scalars or collection of collections of (nullable) value types
-        // for scalars, TElement of element comparer and list comparer match
-        // for collection of value types, element comparer is typed as, say, IEnumerable<int> whereas TElement of list comparer
-        // is something like int[] - those are still compatible with the typed overload
-        // Func<IEnumerable<int>, IEnumerable<int>, bool> is a valid argument for Func<int[], int[], bool>
         return Expression.Lambda<Func<object?, object?, bool>>(
             Expression.Call(
-                TypedCompareMethod,
+                CompareMethod,
                 prm1,
                 prm2,
-                elementComparer.EqualsExpression),
+                Expression.Lambda(
+                    newEqualsExpressionBody,
+                    newInnerPrm1,
+                    newInnerPrm2)),
             prm1,
             prm2);
     }
@@ -131,7 +107,9 @@ public sealed class ListOfReferenceTypesComparer<TConcreteList, TElement> : Valu
     {
         var prm = Expression.Parameter(typeof(object), "o");
 
-        if (elementComparer is ValueComparer<object>)
+        var expectedSignature = typeof(Func<TElement, int>);
+        var actualSignature = typeof(Func<,>).MakeGenericType(elementComparer.Type, typeof(int));
+        if (expectedSignature.IsAssignableFrom(actualSignature))
         {
             return Expression.Lambda<Func<object, int>>(
                 Expression.Call(
@@ -143,34 +121,22 @@ public sealed class ListOfReferenceTypesComparer<TConcreteList, TElement> : Valu
                 prm);
         }
 
-        if (typeof(TElement) == typeof(object))
-        {
-            var newInnerPrm = Expression.Parameter(typeof(object), "o");
+        var newInnerPrm = Expression.Parameter(typeof(TElement), "o");
 
-            var newInnerBody = ReplacingExpressionVisitor.Replace(
-                elementComparer.HashCodeExpression.Parameters[0],
-                Expression.Convert(newInnerPrm, elementComparer.Type),
-                elementComparer.HashCodeExpression.Body);
-
-            return Expression.Lambda<Func<object, int>>(
-                Expression.Call(
-                    GetHashCodeMethod,
-                    Expression.Convert(
-                        prm,
-                        typeof(IEnumerable)),
-                    Expression.Lambda(
-                        newInnerBody,
-                        newInnerPrm)),
-                prm);
-        }
+        var newInnerBody = ReplacingExpressionVisitor.Replace(
+            elementComparer.HashCodeExpression.Parameters[0],
+            Expression.Convert(newInnerPrm, elementComparer.Type),
+            elementComparer.HashCodeExpression.Body);
 
         return Expression.Lambda<Func<object, int>>(
             Expression.Call(
-                TypedGetHashCodeMethod,
+                GetHashCodeMethod,
                 Expression.Convert(
                     prm,
                     typeof(IEnumerable)),
-                    elementComparer.HashCodeExpression),
+                Expression.Lambda(
+                    newInnerBody,
+                    newInnerPrm)),
             prm);
     }
 
@@ -178,7 +144,9 @@ public sealed class ListOfReferenceTypesComparer<TConcreteList, TElement> : Valu
     {
         var prm = Expression.Parameter(typeof(object), "source");
 
-        if (elementComparer is ValueComparer<object>)
+        var expectedSignature = typeof(Func<TElement, TElement>);
+        var actualSignature = typeof(Func<,>).MakeGenericType(elementComparer.Type, elementComparer.Type);
+        if (expectedSignature.IsAssignableFrom(actualSignature))
         {
             return Expression.Lambda<Func<object, object>>(
                 Expression.Call(
@@ -188,69 +156,27 @@ public sealed class ListOfReferenceTypesComparer<TConcreteList, TElement> : Valu
                 prm);
         }
 
-        if (typeof(TElement) == typeof(object))
-        {
-            var newInnerPrm = Expression.Parameter(typeof(object), "source");
+        var newInnerPrm = Expression.Parameter(typeof(TElement), "source");
 
-            var newInnerBody = ReplacingExpressionVisitor.Replace(
-                elementComparer.SnapshotExpression.Parameters[0],
-                Expression.Convert(newInnerPrm, elementComparer.Type),
-                elementComparer.SnapshotExpression.Body);
+        var newInnerBody = ReplacingExpressionVisitor.Replace(
+            elementComparer.SnapshotExpression.Parameters[0],
+            Expression.Convert(newInnerPrm, elementComparer.Type),
+            elementComparer.SnapshotExpression.Body);
 
-            // note we need to also convert the result of inner lambda back to object
-            return Expression.Lambda<Func<object, object>>(
-                Expression.Call(
-                    SnapshotMethod,
-                    prm,
-                    Expression.Lambda(
-                        Expression.Convert(
-                            newInnerBody,
-                            typeof(object)),
-                        newInnerPrm)),
-                prm);
-        }
-
-        // special case for for list of (nullable) value types
-        // TElement of the element comparer is typed as say, IEnumerable<int>
-        // while the TElement of the list comparer would be something like int[]
-        // this makes snapshot lambdas incompatible - we can't use Func<IEnumerable<int>, IEnumerable<int>>
-        // as argument to a method expecting Func<int[], int[]>
-        // to make it work we need to adjust the return type to Func<int[], IEnumerable<int>>
-        if (elementComparer.GetType() is Type { IsGenericType: true } genericType
-            && genericType.GetGenericTypeDefinition() is Type genericTypeDefinition
-            && (genericTypeDefinition == typeof(ListOfValueTypesComparer<,>)
-                || genericTypeDefinition == typeof(ListOfNullableValueTypesComparer<,>)))
-        {
-            var result = Expression.Lambda<Func<object, object>>(
-                Expression.Call(
-                   TypedSnapshotMethod,
-                    prm,
-                    Expression.Lambda(
-                        Expression.Convert(
-                            elementComparer.SnapshotExpression.Body,
-                            typeof(TElement)),
-                        elementComparer.SnapshotExpression.Parameters)),
-                prm);
-
-            return result;
-        }
-
-        Check.DebugAssert(
-            elementComparer is ValueComparer<TElement>,
-            "Element comparer should be typed as ValueComparer<TElement> at this point.");
-
-        // only thing left is collection of scalars (since we handled ListOfValueTypesComparer
-        // and ListOfNullableValueTypesComparer above) - TElement of element comparer and list comparer
-        // are the same so no comparibility issues between funcs
+        // note we need to also convert the result of inner lambda back to TElement
         return Expression.Lambda<Func<object, object>>(
             Expression.Call(
-                TypedSnapshotMethod,
+                SnapshotMethod,
                 prm,
-                elementComparer.SnapshotExpression),
+                Expression.Lambda(
+                    Expression.Convert(
+                        newInnerBody,
+                        typeof(TElement)),
+                    newInnerPrm)),
             prm);
     }
 
-    private static bool Compare(object? a, object? b, Func<object?, object?, bool> elementCompare)
+    private static bool Compare(object? a, object? b, Func<TElement?, TElement?, bool> elementCompare)
     {
         if (ReferenceEquals(a, b))
         {
@@ -307,76 +233,7 @@ public sealed class ListOfReferenceTypesComparer<TConcreteList, TElement> : Valu
                 typeof(IList<>).MakeGenericType(typeof(TElement)).ShortDisplayName()));
     }
 
-    private static bool TypedCompare(object? a, object? b, Func<TElement?, TElement?, bool> elementCompare)
-    {
-        if (ReferenceEquals(a, b))
-        {
-            return true;
-        }
-
-        if (a is null)
-        {
-            return b is null;
-        }
-
-        if (b is null)
-        {
-            return false;
-        }
-
-        if (a is IList<TElement?> aList && b is IList<TElement?> bList)
-        {
-            if (aList.Count != bList.Count)
-            {
-                return false;
-            }
-
-            for (var i = 0; i < aList.Count; i++)
-            {
-                var (el1, el2) = (aList[i], bList[i]);
-                if (el1 is null)
-                {
-                    if (el2 is null)
-                    {
-                        continue;
-                    }
-
-                    return false;
-                }
-
-                if (el2 is null)
-                {
-                    return false;
-                }
-
-                if (!elementCompare(el1, el2))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        throw new InvalidOperationException(
-            CoreStrings.BadListType(
-                (a is IList<TElement?> ? b : a).GetType().ShortDisplayName(),
-                typeof(IList<>).MakeGenericType(typeof(TElement)).ShortDisplayName()));
-    }
-
-    private static int GetHashCode(IEnumerable source, Func<object?, int> elementGetHashCode)
-    {
-        var hash = new HashCode();
-
-        foreach (var el in source)
-        {
-            hash.Add(el == null ? 0 : elementGetHashCode(el));
-        }
-
-        return hash.ToHashCode();
-    }
-
-    private static int TypedGetHashCode(IEnumerable source, Func<TElement?, int> elementGetHashCode)
+    private static int GetHashCode(IEnumerable source, Func<TElement?, int> elementGetHashCode)
     {
         var hash = new HashCode();
 
@@ -388,42 +245,7 @@ public sealed class ListOfReferenceTypesComparer<TConcreteList, TElement> : Valu
         return hash.ToHashCode();
     }
 
-    private static IList<TElement?> Snapshot(object source, Func<object?, object?> elementSnapshot)
-    {
-        if (source is not IList<TElement?> sourceList)
-        {
-            throw new InvalidOperationException(
-                CoreStrings.BadListType(
-                    source.GetType().ShortDisplayName(),
-                    typeof(IList<>).MakeGenericType(typeof(TElement)).ShortDisplayName()));
-        }
-
-        if (IsArray)
-        {
-            var snapshot = new TElement?[sourceList.Count];
-            for (var i = 0; i < sourceList.Count; i++)
-            {
-                var instance = sourceList[i];
-                snapshot[i] = instance == null ? null : (TElement?)elementSnapshot(instance);
-            }
-
-            return snapshot;
-        }
-        else
-        {
-            var snapshot = IsReadOnly ? new List<TElement?>() : (IList<TElement?>)Activator.CreateInstance<TConcreteList>()!;
-            foreach (var e in sourceList)
-            {
-                snapshot.Add(e == null ? null : (TElement?)elementSnapshot(e));
-            }
-
-            return IsReadOnly
-                ? (IList<TElement?>)Activator.CreateInstance(typeof(TConcreteList), snapshot)!
-                : snapshot;
-        }
-    }
-
-    private static IList<TElement?> TypedSnapshot(object source, Func<TElement?, TElement?> elementSnapshot)
+    private static IList<TElement?> Snapshot(object source, Func<TElement?, TElement?> elementSnapshot)
     {
         if (source is not IList<TElement?> sourceList)
         {

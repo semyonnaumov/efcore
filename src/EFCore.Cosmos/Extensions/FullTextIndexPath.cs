4d1d0fb9bffd8e29f4b0e1181b62364b4eb52e0d
maumar <maumar@microsoft.com>
2025-03-18 18:12:26 -0700

    gf

---
+++ src/EFCore.Cosmos/Extensions/FullTextIndexPath.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.Azure.Cosmos;
+
+[Experimental(EFDiagnostics.CosmosFullTextSearchExperimental)]
+internal sealed class FullTextIndexPath
+{
+    public string? Path { get; set; }
+}
